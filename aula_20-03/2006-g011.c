e106b3edcc9ee0f2db65ce977d226955887468bd
Gabriel leite Silva <gabrieleitesilvax@gmail.com>
2026-03-20 11:15:58 -0300

    ie

--- aula_20-03/2006-g011.c
+++ aula_20-03/2006-g011.c
@@ -14,6 +14,10 @@ typedef struct {
     int index;
 } result;
 
+void imprimePop(pop p) {
+    printf("[Index: %d | Valor: %.4f] ", p.index, p.valor);
+}
+
 pop **iniciaMatriz(int *n, result **resultado){
     pop **matriz = malloc(2 * sizeof(pop *));
     for (int i = 0; i < 2; i++)
@@ -23,8 +27,8 @@ pop **iniciaMatriz(int *n, result **resultado){
 
 
     for (int i = 0; i < *n; i++){
-        matriz[0][i].valor = 13.0 + ((double)rand() / RAND_MAX) * 87.0; matriz[0][i].index = i;
-        matriz[1][i].valor = ((double)rand() / RAND_MAX) * 100;  matriz[1][i].index = i;
+        matriz[0][i].valor = ((double)rand() / RAND_MAX) * 2.0 - 1.0;; matriz[0][i].index = i;
+        matriz[1][i].valor = ((double)rand() / RAND_MAX) * 2.0 - 1.0;;  matriz[1][i].index = i;
 
 
         (*resultado)[i].pais = malloc(2 * sizeof(pop));
@@ -32,40 +36,36 @@ pop **iniciaMatriz(int *n, result **resultado){
     
     return matriz;
 }
-
-result verificaResultado(result *r, pop **m, int *n){
-    double g1,g2;
-    
+void soma(result *r, pop **m, int *n){
     for (int i = 0; i < *n; i++){
         double x1 = m[0][i].valor;
         double x2 = m[1][i].valor;
 
-        g1 = - pow((x1 - 5), 2) - pow((x2 - 5), 5) + 100;
-        g2 = pow((x1 - 6), 2) + pow((x2 - 5), 5) - 82.81;
-
-        double penalizacao = (g1 + g2) * 1000;
-        r[i].valor += penalizacao;
+        r[i].valor = (x1 * x1) + pow((x2 - 1.0), 2);
+        r[i].index = i; 
+        r[i].pais[0].valor = x1; r[i].pais[1].valor = x2;
     }
-    
+}
+result verificaResultado(result *r, pop **m, int *n){
+    double h;
+
+    for (int i = 0; i < *n; i++){
+        double x1 = m[0][i].valor;
+        double x2 = m[1][i].valor;
+
+       h = x2 - (x1 * x1);
+        if(fabs(h) > 0.0001) { 
+            r[i].valor += fabs(h) * 10000.0;
+        }
+    }
     result menor = r[0];
     for (int i = 1; i < *n; i++){ 
         if(r[i].valor < menor.valor)
             menor = r[i];
     }
     return menor;
+       
 }
-
-void soma(result *r, pop **m, int *n){
-    for(int i = 0; i < *n; i++){
-        double x1 = m[0][i].valor;
-        double x2 = m[1][i].valor;
-        
-        r[i].valor = pow((x1 - 10), 3) + pow((x2 - 20), 3);
-        r[i].index = i;
-        r[i].pais[0] = m[0][i]; r[i].pais[1] = m[1][i]; 
-    }
-}
-
 void liberaMemoria(pop **m, result *r, int *n) {
     if (r != NULL) {
         for (int i = 0; i < *n; i++) {
@@ -80,18 +80,18 @@ void liberaMemoria(pop **m, result *r, int *n) {
         free(m);
     }
 }
-
 int main(){
-    int n = 10000;
+    int n = 100000;
     result *r;
-    int count = 0;
     srand(time(NULL));
-    for (int i = 0; i < 10; i++){
-        pop **m = iniciaMatriz(&n, &r);
-        soma(r,m, &n);
-        result final = verificaResultado(r,m,&n);
-        printf("resultado: valor -> %lf; pais: %lf, %lf; index: %d \n", final.valor, 
-            final.pais[0].valor, final.pais[1].valor, final.index);
-        liberaMemoria(m, r, &n);
-    }   
+
+    pop **m = iniciaMatriz(&n, &r);
+
+
+    soma(r,m, &n);
+    result final = verificaResultado(r,m,&n);
+
+    printf("resultado: valor -> %lf; pais: %lf, %lf; index: %d \n", final.valor, 
+        final.pais[0].valor, final.pais[1].valor, final.index);
+    liberaMemoria(m, r, &n);
 }
